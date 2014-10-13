7fd7135dd7d806a7d87bb8dd9835d9260ee30108
ljleppan <leo.leppanen@helsinki.fi>
2014-10-13 11:09:37 +0300

    Exceptions

---
+++ src/main/java/fi/helsinki/cs/tmc/core/exception/UserVisibleException.java
@@ -0,0 +1,25 @@
+package fi.helsinki.cs.tmc.core.exception;
+
+
+public abstract class UserVisibleException extends TmcException {
+
+    public UserVisibleException() {
+
+        super();
+    }
+
+    public UserVisibleException(final String message, final Throwable cause) {
+
+        super(message, cause);
+    }
+
+    public UserVisibleException(final String message) {
+
+        super(message);
+    }
+
+    public UserVisibleException(final Throwable cause) {
+
+        super(cause);
+    }
+}
