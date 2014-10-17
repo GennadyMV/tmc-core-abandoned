36a49bda8d5642fb49f512f94da92536b5a17754
ljleppan <leo.leppanen@helsinki.fi>
2014-10-17 17:18:22 +0300

    Core can now provide Settings and an ExecutorService. Settings serves as the transportation vehicle between Core and UI for client settings. Executor service allows for using a common thread pool further down the line.

--- src/main/java/fi/helsinki/cs/tmc/client/core/Core.java
+++ src/main/java/fi/helsinki/cs/tmc/client/core/Core.java
@@ -1,18 +1,33 @@
-package fi.helsinki.cs.tmc.core;
+package fi.helsinki.cs.tmc.client.core;
 
-import fi.helsinki.cs.tmc.core.ui.UIInvoker;
+import fi.helsinki.cs.tmc.client.core.ui.UIInvoker;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class Core {
     
     private static UIInvoker uiInvoker;
+    private static Settings settings;
     
-    public static void initialize(final UIInvoker uiInvoker) {
+    public static void initialize(final UIInvoker uiInvoker, final Settings settings) {
         
         Core.uiInvoker = uiInvoker;
+        Core.settings = settings;
     }
     
     public static UIInvoker getUIInvoker() {
         
         return Core.uiInvoker;
+    }
+
+    public static Settings getSettings() {
+
+        return Core.settings;        
+    }
+    
+    public static ExecutorService getExecutorService() {
+        
+        return Executors.newSingleThreadExecutor();
     }
 }
---
+++ src/main/java/fi/helsinki/cs/tmc/client/core/Settings.java
@@ -0,0 +1,21 @@
+package fi.helsinki.cs.tmc.client.core;
+
+import fi.helsinki.cs.tmc.client.core.domain.Course;
+
+public interface Settings {
+    
+    String tmcServerBaseUrl();
+
+    String tmcApiVersion();
+
+    String clientId();
+
+    String clientVersion();
+    
+    Course activeCourse();
+
+    String password();
+
+    String username();
+
+}
