4ea24b6743e4f023cfd513e0b3de8c9edf723cc0
ljleppan <leo.leppanen@helsinki.fi>
2014-10-27 13:24:26 +0200

    Tests for Zipper

---
+++ src/test/java/fi/helsinki/cs/tmc/client/core/io/zip/ZipperTest.java
@@ -0,0 +1,119 @@
+package fi.helsinki.cs.tmc.client.core.io.zip;
+
+import fi.helsinki.cs.tmc.client.core.io.zip.decider.ZipEverythingDecider;
+import fi.helsinki.cs.tmc.client.core.io.zip.decider.ZippingDecider;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.junit.Assert.*;
+
+public class ZipperTest {
+
+    private static final String SLASH = File.separator;
+
+    @Rule
+    public TemporaryFolder temporaryDirectory = new TemporaryFolder();
+
+    private String mainDirectory;
+
+    @Before
+    public void setUp() throws IOException {
+
+        mainDirectory = temporaryDirectory.getRoot().getAbsolutePath() + SLASH + "MyExercise";
+        new File(mainDirectory + SLASH + "src" + SLASH + "subdir").mkdirs();
+        new File(mainDirectory + SLASH + "src" + SLASH + "Included1.txt").createNewFile();
+        new File(mainDirectory + SLASH + "src" + SLASH + "subdir" + SLASH + "Included2.txt").createNewFile();
+
+        new File(mainDirectory + SLASH + "Excluded.txt").createNewFile();
+        new File(mainDirectory + SLASH + "test").mkdir();
+        new File(mainDirectory + SLASH + "test" + SLASH + "Excluded.txt").createNewFile();
+        new File(mainDirectory + SLASH + "test").mkdir();
+        new File(mainDirectory + SLASH + "excluded").mkdir();
+        new File(mainDirectory + SLASH + "excluded" + SLASH + "Foo.txt").createNewFile();
+    }
+
+    @Test(expected = IOException.class)
+    public void failsOnNonexistantRootDirectory() throws IOException {
+
+        final Zipper zipper = new Zipper(new File(mainDirectory + SLASH + "noSuchFolder"), new ZipEverythingDecider());
+        zipper.zipProjectSources();
+    }
+
+    @Test(expected = IOException.class)
+    public void failsOnNonDirectoryRoot() throws IOException {
+
+        final File file = temporaryDirectory.newFile();
+
+        final Zipper zipper = new Zipper(file, new ZipEverythingDecider());
+        zipper.zipProjectSources();
+    }
+
+    @Test
+    public void itShouldZipRecursivelyFilesThatZipDeciderSelects() throws IOException {
+
+        final ZippingDecider decider = new ZippingDecider() {
+            @Override
+            public boolean shouldZip(final String relativeZipPath) {
+                return !relativeZipPath.equals("MyExercise/Excluded.txt") &&
+                        !relativeZipPath.equals("MyExercise/src/Excluded.txt") &&
+                        !relativeZipPath.equals("MyExercise/excluded/");
+            }
+        };
+
+        final List<String> entries = getZipEntries(decider);
+
+        if (!entries.contains("MyExercise/src/Included1.txt")) {
+            fail("Expected file not in zip.");
+        }
+        if (!entries.contains("MyExercise/src/subdir/Included2.txt")) {
+            fail("Expected file not in zip.");
+        }
+        if (entries.contains("MyExercise/Excluded.txt")) {
+            fail("File that was supposed to be excluded was found in the zip.");
+        }
+        if (entries.contains("MyExercise/src/Excluded.txt")) {
+            fail("File that was supposed to be excluded was found in the zip.");
+        }
+        if (entries.contains("MyExercise/excluded/Foo.txt")) {
+            fail("File that was supposed to be excluded was found in the zip.");
+        }
+    }
+
+    private List<String> getZipEntries(final ZippingDecider decider) throws IOException {
+
+        final Zipper zipper = new Zipper(new File(mainDirectory), decider);
+        final byte[] zipData = zipper.zipProjectSources();
+
+        return zipEntryNames(zipData);
+    }
+
+    private List<String> zipEntryNames(final byte[] zipData) throws IOException {
+
+        final List<String> result = new ArrayList<>();
+
+        final ZipInputStream zipInputStream = new ZipInputStream(new ByteArrayInputStream(zipData));
+        try {
+
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                result.add(zipEntry.getName());
+            }
+
+        } finally {
+            zipInputStream.close();
+        }
+
+        return result;
+    }
+}
